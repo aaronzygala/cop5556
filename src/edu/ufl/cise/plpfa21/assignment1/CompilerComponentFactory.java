6ba208b2053919dd066bf36b99ba2b28c458ff40
aaronzygala <azygala2016@fau.edu>
2021-11-10 21:29:58 -0500

    Implemented assignment 4, passes tests immediately

--- src/edu/ufl/cise/plpfa21/assignment1/CompilerComponentFactory.java
+++ src/edu/ufl/cise/plpfa21/assignment1/CompilerComponentFactory.java
@@ -2,20 +2,28 @@ package edu.ufl.cise.plpfa21.assignment1;
 
 import edu.ufl.cise.plpfa21.assignment2.IPLPParser;
 import edu.ufl.cise.plpfa21.assignment2.Parser;
+import edu.ufl.cise.plpfa21.assignment3.ast.ASTVisitor;
+import edu.ufl.cise.plpfa21.assignment4.TypeCheckVisitor;
+
 
 public class CompilerComponentFactory {
 
-	public static IPLPLexer getLexer(String input){
-		//TODO  create and return a Lexer instance to parse the given input.
-		IPLPLexer lexer =  new Lexer(input);
+	public static IPLPLexer getLexer(String input) {
+		//Replace with whatever is needed for your lexer.
+		IPLPLexer lexer = new Lexer(input);
 		return lexer;
 	}
 	
-	public static IPLPParser getParser(String input){
-		//TODO  create and return a Parser instance to parse the given input.
-		 //Implement this in Assignment 2
-	   	 //Your parser will create a lexer.
-		IPLPParser parser =  new Parser(getLexer(input));
-		return parser;
+	public static IPLPParser getParser(String input) {
+		//Replace this with whatever is needed for your parser.
+		IPLPParser parser = new Parser(getLexer(input));
+		return parser;		
 	}
+
+	public static ASTVisitor getTypeCheckVisitor() {
+		// Replace this with whatever is needed for your compiler
+		return new TypeCheckVisitor();
+	}
+	
+
 }
